00fc1d32b1312ee12158ae704ce13d7e8607952b
iyeong-gyo <whdnseowkd@naver.com>
2020-06-28 23:10:57 +0900

    first commit

---
+++ Samel-1/src/main/java/sample/Samel1Application.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Samel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Samel1Application.class, args);
+	}
+
+}
